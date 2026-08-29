bf462066883a61d04ec68518577ce7e453c97c84
Matt Whilden <matthew.whilden+osm@gmail.com>
2026-08-29 01:19:34 -0700

    fix the about screen crashing on iOS
    getRatingUri had a TODO() in a string template, which throws when the About
    screen composes it - not when the entry is tapped. The app is not on the App
    Store yet, and the interface already documents null as "no store to rate in",
    which is what it is.

--- app/src/iosMain/kotlin/de/westnordost/streetcomplete/screens/about/IosAppStoreInfo.kt
+++ app/src/iosMain/kotlin/de/westnordost/streetcomplete/screens/about/IosAppStoreInfo.kt
@@ -1,8 +1,11 @@
 package de.westnordost.streetcomplete.screens.about
 
 object IosAppStoreInfo : AppStoreInfo {
-    override fun getRatingUri(): String =
-        "https://apps.apple.com/app/id${TODO("insert StreetComplete app id")}?action=write-review"
+    /** null because the app is not on the App Store yet, so there is nothing to rate it in.
+     *  Once it is, this is
+     *  "https://apps.apple.com/app/id<the app id>?action=write-review" */
+    override fun getRatingUri(): String? =
+        null
 
     override fun disallowsInAppDonationLinks(): Boolean =
         true
